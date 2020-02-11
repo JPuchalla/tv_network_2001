73ab127d5a3adcce6a11d38faaa87b0ebe5d7351
Jack Puchalla <Jackpuchallalab@gmail.com>
2020-02-11 10:03:54 -0700

    iteration 1 complete

--- lib/character.rb
+++ lib/character.rb
@@ -1,1 +1,21 @@
+class Character
 
+  attr_reader :char
+
+  def initialize(char)
+    @char = char
+  end
+
+  def name
+    @char[:name]
+  end
+
+  def actor
+    @char[:actor]
+  end
+
+  def salary
+    @char[:salary]
+  end
+
+end
