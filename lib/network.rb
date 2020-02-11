aeb847029bb00aa794dda75210b222433969b8e7
Jack Puchalla <Jackpuchallalab@gmail.com>
2020-02-11 10:34:25 -0700

    created network test and class with two passing tests

--- lib/network.rb
+++ lib/network.rb
@@ -1,1 +1,17 @@
+require './lib/character.rb'
+require './lib/show.rb'
 
+class Network
+
+  attr_reader :name, :shows
+
+  def initialize(name, shows)
+    @name = name
+    @shows = []
+  end
+
+  def add_show
+    @shows << show
+  end
+
+end
